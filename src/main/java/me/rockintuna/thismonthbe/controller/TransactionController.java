e2d6fbe9155631b9914e95af4c941c49b643fd08
rockintuna <rockintuna@naver.com>
2025-03-09 16:29:23 +0900

    feat: Controller 생성

--- src/main/java/me/rockintuna/thismonthbe/controller/TransactionController.java
+++ src/main/java/me/rockintuna/thismonthbe/controller/TransactionController.java
@@ -1,8 +1,34 @@
 package me.rockintuna.thismonthbe.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import me.rockintuna.thismonthbe.dto.TransactionResponseDto;
+import me.rockintuna.thismonthbe.dto.TransactionRequestDto;
+import me.rockintuna.thismonthbe.service.TransactionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class TransactionController {
 
+    private final TransactionService transactionService;
+
+    @GetMapping("/transactions")
+    public ResponseEntity<List<TransactionResponseDto>> transactions(
+            @RequestParam Integer year,
+            @RequestParam Integer month
+    ) {
+        return ResponseEntity.ok(transactionService.getTransactions(year, month));
+    }
+
+    @PostMapping("/transactions")
+    public ResponseEntity<Void> transactions(
+            @RequestBody TransactionRequestDto requestDto
+    ) {
+        transactionService.addTransactions(requestDto);
+        return ResponseEntity.ok().build();
+    }
 }
--- src/main/java/me/rockintuna/thismonthbe/service/TransactionService.java
+++ src/main/java/me/rockintuna/thismonthbe/service/TransactionService.java
@@ -1,7 +1,17 @@
 package me.rockintuna.thismonthbe.service;
 
+import me.rockintuna.thismonthbe.dto.TransactionResponseDto;
+import me.rockintuna.thismonthbe.dto.TransactionRequestDto;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TransactionService {
+    public List<TransactionResponseDto> getTransactions(Integer year, Integer month) {
+        return null;
+    }
+
+    public void addTransactions(TransactionRequestDto requestDto) {
+    }
 }
