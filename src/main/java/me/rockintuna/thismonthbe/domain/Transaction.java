b1ca04c3f83f80ecd8ce7dad84dbfd0aab2bf012
rockintuna <rockintuna@naver.com>
2025-03-09 15:17:40 +0900

    feat: 도메인 클래스 추가 [Transaction, User]

---
+++ src/main/java/me/rockintuna/thismonthbe/domain/Transaction.java
@@ -0,0 +1,25 @@
+package me.rockintuna.thismonthbe.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private Long amount;
+
+    private Long userId;
+}
---
+++ src/main/java/me/rockintuna/thismonthbe/domain/User.java
@@ -0,0 +1,13 @@
+package me.rockintuna.thismonthbe.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class User {
+    private Long id;
+    private String username;
+}
