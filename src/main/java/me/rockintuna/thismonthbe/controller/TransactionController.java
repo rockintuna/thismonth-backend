7f63c4b9458adf954b1bfbaeb21728cc28cc0ad9
rockintuna <rockintuna@naver.com>
2025-03-09 15:20:00 +0900

    feat: Transaction Controller, Service 클래스 추가

---
+++ src/main/java/me/rockintuna/thismonthbe/controller/TransactionController.java
@@ -0,0 +1,8 @@
+package me.rockintuna.thismonthbe.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TransactionController {
+
+}
---
+++ src/main/java/me/rockintuna/thismonthbe/service/TransactionService.java
@@ -0,0 +1,7 @@
+package me.rockintuna.thismonthbe.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionService {
+}
