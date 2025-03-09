86b8fbb4015218b33f32eb075b7615fc6e536bf3
rockintuna <rockintuna@naver.com>
2025-03-09 14:45:48 +0900

    first commit

---
+++ src/main/java/me/rockintuna/thismonthbe/ThisMonthBeApplication.java
@@ -0,0 +1,13 @@
+package me.rockintuna.thismonthbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThisMonthBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThisMonthBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/rockintuna/thismonthbe/ThisMonthBeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.rockintuna.thismonthbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThisMonthBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
