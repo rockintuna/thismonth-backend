1fed91820960ed88f43ad30eb5abe87d8a8b9f95
rockintuna <rockintuna@naver.com>
2025-03-09 21:19:15 +0900

    feat: transaction 서비스 구현

--- src/main/java/me/rockintuna/thismonthbe/repositosy/TransactionRepository.java
+++ src/main/java/me/rockintuna/thismonthbe/repositosy/TransactionRepository.java
@@ -2,6 +2,13 @@ package me.rockintuna.thismonthbe.repositosy;
 
 import me.rockintuna.thismonthbe.domain.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    @Query("select t from Transaction t " +
+            "join fetch User u " +
+            "where t.year = :year and t.month = :month")
+    List<Transaction> findAllByYearAndMonth(Integer year, Integer month);
 }
--- src/main/java/me/rockintuna/thismonthbe/service/TransactionService.java
+++ src/main/java/me/rockintuna/thismonthbe/service/TransactionService.java
@@ -1,17 +1,35 @@
 package me.rockintuna.thismonthbe.service;
 
+import lombok.RequiredArgsConstructor;
+import me.rockintuna.thismonthbe.domain.Transaction;
+import me.rockintuna.thismonthbe.domain.User;
 import me.rockintuna.thismonthbe.dto.TransactionResponseDto;
 import me.rockintuna.thismonthbe.dto.TransactionRequestDto;
+import me.rockintuna.thismonthbe.repositosy.TransactionRepository;
+import me.rockintuna.thismonthbe.repositosy.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class TransactionService {
+
+    private final TransactionRepository transactionRepository;
+    private final UserRepository userRepository;
+
     public List<TransactionResponseDto> getTransactions(Integer year, Integer month) {
-        return null;
+        List<Transaction> transactionList = transactionRepository.findAllByYearAndMonth(year, month);
+
+        return transactionList.stream().map(TransactionResponseDto::of).toList();
     }
 
-    public void addTransactions(TransactionRequestDto requestDto) {
+    public TransactionResponseDto addTransactions(TransactionRequestDto requestDto) {
+        User user = userRepository.findById(requestDto.getUserId()).orElseThrow(
+                () -> new RuntimeException("User with id " + requestDto.getUserId() + " not found")
+        );
+
+        return TransactionResponseDto.of(
+                transactionRepository.save(Transaction.create(requestDto, user)));
     }
 }
