b67df2c10424b859257e104921f475735b55bfc1
rockintuna <rockintuna@naver.com>
2025-03-12 20:38:22 +0900

    fix: transaction 목록 조회 시 amount 역순 정렬

--- src/main/java/me/rockintuna/thismonthbe/repositosy/TransactionRepository.java
+++ src/main/java/me/rockintuna/thismonthbe/repositosy/TransactionRepository.java
@@ -9,6 +9,7 @@ import java.util.List;
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
     @Query("select t from Transaction t " +
             "join fetch t.user " +
-            "where t.year = :year and t.month = :month")
+            "where t.year = :year and t.month = :month " +
+            "order by t.amount desc")
     List<Transaction> findAllByYearAndMonth(Integer year, Integer month);
 }
