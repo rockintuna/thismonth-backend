96a97c1cb83dc119f6e31ed6cd2944bb935dec18
rockintuna <rockintuna@naver.com>
2025-03-17 22:41:38 +0900

    fix: tx response에 id 추가

--- src/main/java/me/rockintuna/thismonthbe/dto/TransactionResponseDto.java
+++ src/main/java/me/rockintuna/thismonthbe/dto/TransactionResponseDto.java
@@ -11,12 +11,14 @@ import me.rockintuna.thismonthbe.domain.Transaction;
 @Getter
 @Builder
 public class TransactionResponseDto {
+    private Long id;
     private String title;
     private Long amount;
     private String userName;
 
     public static TransactionResponseDto of(Transaction transaction) {
         return TransactionResponseDto.builder()
+                .id(transaction.getId())
                 .title(transaction.getTitle())
                 .amount(transaction.getAmount())
                 .userName(transaction.getUser().getUsername())
